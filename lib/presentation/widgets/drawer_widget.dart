162152aa3f711dd09ad5b8575e02bbab1ce48248
tofayelahmmedbabu <tofayelahmmedbabu@gmail.com>
2024-10-05 11:08:02 +0600

    fixed drawer widget

--- lib/presentation/widgets/drawer_widget.dart
+++ lib/presentation/widgets/drawer_widget.dart
@@ -1,4 +1,7 @@
 import 'package:eyesofcosmos/presentation/srceens/IFrameView.dart';
+import 'package:eyesofcosmos/presentation/srceens/privacy_policy/aboutUs.dart';
+import 'package:eyesofcosmos/presentation/srceens/privacy_policy/policy.dart';
+import 'package:eyesofcosmos/presentation/srceens/privacy_policy/resources.dart';
 import 'package:eyesofcosmos/presentation/utils/image_assets.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -14,107 +17,122 @@ class _DrawerWidgetState extends State<DrawerWidget> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        backgroundColor: Colors.black,
-        child: ListView(
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(ImageAssets.onBoardingBackground1Jpg),
-                  fit: BoxFit.contain,
-                ),
+      backgroundColor: Colors.black,
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(ImageAssets.onBoardingBackground1Jpg),
+                fit: BoxFit.contain,
               ),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Text(
-                    'Eye of Cosmos',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
+            ),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Text(
+                  'Eye of The Cosmos',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
             ),
-            ListTile(
-              title: const Text('Music Changer'),
-              textColor: Colors.white,
-              onTap: () {
-                // Update the state of the app
-                // Then close the drawer
-                Navigator.pop(context);
-              },
+          ),
+          // ListTile(
+          //   title: const Text('Music Changer'),
+          //   textColor: Colors.white,
+          //   onTap: () {
+          //     // Update the state of the app
+          //     // Then close the drawer
+          //     Navigator.pop(context);
+          //   },
+          // ),
+          ListTile(
+            title: const Text('Resources'),
+            textColor: Colors.white,
+            onTap: () {
+              Navigator.pop(context); // Close the drawer first
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => buildResourcesPage(context),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text("About us"),
+            textColor: Colors.white,
+            onTap: () {
+              Navigator.pop(context); // Close the drawer first
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => buildTeamArtemisPage(context),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Terms and Policy'),
+            textColor: Colors.white,
+            onTap: () {
+              Navigator.pop(context); // Close the drawer first
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => buildTermsAndPolicyPage(context),
+                ),
+              );
+            },
+          ),
+
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                    icon: const Icon(
+                      Icons.facebook,
+                    ),
+                    color: Colors.blue,
+                    onPressed: () => {
+                          Get.to(WebviewScreen(
+                              link: "https://rakibhasan.me",
+                              onInitFunction: () {}))
+                        }),
+                IconButton(
+                    icon: const Icon(
+                      Icons.file_download,
+                    ),
+                    color: Colors.blue,
+                    onPressed: () => {
+                          Get.to(WebviewScreen(
+                              link:
+                                  "https://drive.google.com/file/d/11WBlYiL4OcP6Cj9_SwFRH4HUm94W3rqR/view?usp=drivesdk"))
+                        }),
+                IconButton(
+                    icon: Image.asset(
+                      ImageAssets.githubLogo,
+                      width: 25,
+                      height: 25,
+                      color: Colors.white,
+                    ),
+                    color: Colors.blue,
+                    onPressed: () => {
+                          Get.to(WebviewScreen(
+                              link: "https://github.com/ma5bah/EyeOfTheCosmos"))
+                        })
+              ],
             ),
-            ListTile(
-              title: const Text('Resources'),
-              textColor: Colors.white,
-              onTap: () {
-                // Update the state of the app
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Terms and Policy'),
-              textColor: Colors.white,
-              onTap: () {
-                // Update the state of the app
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text("About us"),
-              textColor: Colors.white,
-              onTap: () {
-                // Update the state of the app
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  IconButton(
-                      icon: const Icon(
-                        Icons.facebook,
-                      ),
-                      color: Colors.blue,
-                      onPressed: () => {
-                            Get.to(WebviewScreen(link: "https://rakibhasan.me",onInitFunction: (){}))
-                          }),
-                  IconButton(
-                      icon: const Icon(
-                        Icons.file_download,
-                      ),
-                      color: Colors.blue,
-                      onPressed: () => {
-                            Get.to(WebviewScreen(
-                                link:
-                                    "https://drive.google.com/file/d/11WBlYiL4OcP6Cj9_SwFRH4HUm94W3rqR/view?usp=drivesdk"))
-                          }),
-                  IconButton(
-                      icon: Image.asset(
-                        ImageAssets.githubLogo,
-                        width: 25,
-                        height: 25,
-                        color: Colors.white,
-                      ),
-                      color: Colors.blue,
-                      onPressed: () => {
-                            Get.to(WebviewScreen(
-                                link: "https://github.com/ma5bah/EyesOfCosmos"))
-                          })
-                ],
-              ),
-            ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
